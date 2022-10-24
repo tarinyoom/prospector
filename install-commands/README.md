7f5af845ca7b35ee5828e21f93d9bc216d691485
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-10-24 06:47:24 -0700

    install commands

---
+++ install-commands/README.md
@@ -0,0 +1,3 @@
+# Install Commands
+
+<p>To run, make sure node is installed, then run `npm install`, then run `npm start`. The uploaded commands are specified in `src/commands.json`.</p>
--- install-commands/src/index.ts
+++ install-commands/src/index.ts
@@ -1,1 +1,21 @@
-console.log("hellow orld");
+import axios from 'axios';
+import commands from './commands.json';
+import secrets from './secrets.json';
+
+const testUrl = `https://discord.com/api/v10/applications/${secrets.APPLICATION_ID}/guilds/${secrets.TEST_GUILD_ID}/commands`
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bot ${secrets.BOT_TOKEN}`
+}
+
+commands.command_list.forEach(command => {
+  axios.post(testUrl, command, {headers: headers
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+    
+});
