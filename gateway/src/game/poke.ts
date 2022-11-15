182166f38c33d5dbedb1c72a45f2ac033a40e323
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-11-14 22:30:40 -0800

    updates poke interface

--- gateway/src/game/poke.ts
+++ gateway/src/game/poke.ts
@@ -2,14 +2,14 @@
 // TODO, change the type signature here (needs to take in the target string id)
 export async function poke(request: GameRequest) : Promise<GameResponse> {
 	return {
-		"userId": request.userId,
-		"msg": "this is a poke response",
-		"buttons": [
+		userId: request.userId,
+		msg: "this is a poke response",
+		buttons: [
 			{
 				"text": "just a button",
 				"stage": "followup string to send on button callback"
 			}
 		],
-		"userData": null
+		playerData: null
 	};
 }
